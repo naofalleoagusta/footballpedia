77b8f688ee252bb04d75ca3bf1ed1983912bc748
naofalleoagusta <naofal.leo.agusta98@gmail.com>
2023-01-08 12:11:53 +0700

    adjust prod behaviour

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,8 +2,6 @@ const FOOTBALL_API_URL = "https://api.football-data.org/v4/";
 
 export const FOOTBALL_API_TOKEN = import.meta.env.VITE_FOOTBALL_DATA_TOKEN;
 export const ENV = import.meta.env.MODE;
-export const FOOTBALL_API_BASE_URL = `${
-  ENV === "production" ? "" : "https://cors-anywhere.herokuapp.com/"
-}${FOOTBALL_API_URL}`;
+export const FOOTBALL_API_BASE_URL = `${"https://cors-anywhere.herokuapp.com/"}${FOOTBALL_API_URL}`;
 export const NO_CREST_IMG =
   "https://www.pngkey.com/png/detail/43-438494_unknown-crest-crest-clipart.png";
